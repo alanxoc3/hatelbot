b49f59ab30a668447397d9f49781b7eeb3d9d367
Alan Morgan <alanxoc3@gmail.com>
2021-03-24 14:41:36 -0600

    Added GCP function. For cloud functions to call.

--- bot.py
+++ bot.py
@@ -2,6 +2,17 @@
 import video_upload as vu
 import video_create as vc
 
+def runGCP():
+    video_filename = "a_video.mp4"
+    tweet_text = "Testing this musical tweet on GCP!"
+    vc.create_video(video_filename, tweet_text)
+
+    videoTweet = vu.VideoTweet(video_filename)
+    videoTweet.upload_init()
+    videoTweet.upload_append()
+    videoTweet.upload_finalize()
+    videoTweet.tweet(tweet_text)
+
 if __name__ == "__main__":
     video_filename = "a_video.mp4"
     tweet_text = "Testing this musical tweet!"
